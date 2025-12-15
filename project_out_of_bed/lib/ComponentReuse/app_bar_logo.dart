1c51da0c7f04664545ab3df7712b0b87bda53181
Pandaniens <91369306+Pandaniens@users.noreply.github.com>
2025-12-15 19:21:12 +0100

    ForkProjet

---
+++ project_out_of_bed/lib/ComponentReuse/app_bar_logo.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AppBarLogo extends StatelessWidget implements PreferredSizeWidget {
+  const AppBarLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(300),
+      child: AppBar(
+        elevation: 0,
+        titleSpacing: 16,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Texte
+                  Text(
+                    "OUT OF BED",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+
+                  SizedBox(width: 12),
+
+                  // SVG (lit + personnage)
+                  SvgPicture.asset('assets/icons/logo.svg', height: 40),
+
+                  Spacer(),
+
+                  // Bouton carré en haut à droite
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_forward, color: Colors.white),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 2),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
